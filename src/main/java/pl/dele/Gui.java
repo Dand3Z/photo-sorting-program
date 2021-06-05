0c5648d1df9e1c9c83f704e49a390cf127df90da
Daniel Delegacz <ddelegacz@gmail.com>
2021-06-05 13:43:04 +0200

    gui is done

--- src/main/java/pl/dele/Gui.java
+++ src/main/java/pl/dele/Gui.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 
 public class Gui {
@@ -16,10 +18,14 @@ public class Gui {
     public static final String DESTINATION = "Destination";
 
     private JFrame frame;
-    private JPanel panel;
+    private JPanel titlePanel;
+    private JPanel fileChooserPanel;
+    private JPanel threadPanel;
+    private JPanel executePanel;
 
     private String srcDirectoryPath = null;
     private String dstDirectoryPath = null;
+    private int threadAmount = 5;
 
     public Gui() {
         createWindow();
@@ -28,32 +34,82 @@ public class Gui {
     private void createWindow() {
         frame = new JFrame("Photo-sorting-program");
         frame.setLayout(new FlowLayout());
-        panel = new JPanel();
-        panel.setLayout(new FlowLayout());
+        titlePanel = new JPanel();
+        titlePanel.setLayout(new FlowLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 200);
         frame.setLocationRelativeTo(null);
-        frame.add(panel);
-
         createTitleLabel();
+        frame.add(titlePanel);
 
-        JPanel corePanel = new JPanel();
-        corePanel.setLayout(new GridLayout(2,3));
-        createJFileChooser(SOURCE, corePanel);
-        createJFileChooser(DESTINATION, corePanel);
-        frame.add(corePanel);
+        // core panel
+        fileChooserPanel = new JPanel();
+        fileChooserPanel.setLayout(new GridLayout(2,3));
+        createJFileChooser(SOURCE);
+        createJFileChooser(DESTINATION);
+        frame.add(fileChooserPanel);
+
+        // Thread panel
+        threadPanel = new JPanel();
+        createThreadPanel();
+        frame.add(threadPanel);
+
+        // Execute panel
+        executePanel = new JPanel();
+        createExecutePanel();
+        frame.add(executePanel);
 
         frame.setResizable(false);
         frame.setVisible(true);
     }
 
-    private void createTitleLabel() {
-        JLabel titleLabel = new JLabel("A program for sorting photos by the date they were taken ");
-        titleLabel.setBounds(0, 10, 100,10);
-        panel.add(titleLabel);
+    private void createExecutePanel() {
+        executePanel.setLayout(new GridLayout(1,4));
+        JButton button = new JButton("Execute");
+
+        executePanel.add(button);
     }
 
-    private void createJFileChooser(String kind, JPanel panel) {
+    private void createThreadPanel() {
+        JLabel label = new JLabel("Number of threads:");
+        JTextField textField = new JTextField(3);
+        textField.setHorizontalAlignment(JTextField.RIGHT);
+        textField.setText("5");
+
+        textField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                String text = textField.getText();
+                try {
+                    int amount = Integer.parseInt(text);
+                    if (amount > 0) {
+                        threadAmount = amount;
+                    }
+                } catch (NumberFormatException ex) {ex.getMessage();}
+            }
+        });
+
+        threadPanel.add(label);
+        threadPanel.add(textField);
+    }
+
+    private void createTitleLabel() {
+        JLabel titleLabel = new JLabel("A program for sorting photos by the date they were taken ");
+        //titleLabel.setBounds(0, 10, 100,10);
+        titlePanel.add(titleLabel);
+    }
+
+    private void createJFileChooser(String kind) {
 
         JLabel kindLabel = new JLabel(kind +" directory: ");
         JButton button = new JButton(kind);
@@ -73,9 +129,9 @@ public class Gui {
                 }
             }
         });
-        panel.add(kindLabel, BorderLayout.AFTER_LINE_ENDS);
-        panel.add(button);
-        panel.add(selectedFolderLabel, BorderLayout.LINE_END);
+        fileChooserPanel.add(kindLabel, BorderLayout.AFTER_LINE_ENDS);
+        fileChooserPanel.add(button);
+        fileChooserPanel.add(selectedFolderLabel, BorderLayout.LINE_END);
     }
 
 }
